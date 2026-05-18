6ed15eb26b78a67c513bec49d8338d96da6ad2bc
mahdimonir <mahdimoniruzzaman@gmail.com>
2026-05-18 20:58:37 +0600

    feat: add main Go program demonstrating array initialization and length calculation

---
+++ 75_Go/Learn-go/10_Array/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var number [6]int
+
+	number[0] = 10
+	number[1] = 20
+	number[2] = 30
+	number[3] = 40
+	number[4] = 50
+	number[5] = 60
+
+	fmt.Println("Array:", number)
+	fmt.Println("Length of array:", len(number))
+}
