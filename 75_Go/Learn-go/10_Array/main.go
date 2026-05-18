f1f43b411884cbf41366e3ff8e75a33c2f32dcb2
mahdimonir <mahdimoniruzzaman@gmail.com>
2026-05-18 21:02:34 +0600

    refactor: uncomment array and length print statements for clarity

--- 75_Go/Learn-go/10_Array/main.go
+++ 75_Go/Learn-go/10_Array/main.go
@@ -13,6 +13,10 @@ func main() {
 	number[4] = 50
 	number[5] = 60
 
-	fmt.Println("Array:", number)
-	fmt.Println("Length of array:", len(number))
+	// fmt.Println("Array:", number)
+	// fmt.Println("Length of array:", len(number))
+
+	for i := 0; i < len(number); i++ {
+		fmt.Printf("Element at index %d: %d\n", i, number[i])
+	}
 }
